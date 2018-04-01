54f017e40a2bbcfe7d31fa1abb3d73ad16fd6084
smorgenfeld <spencermspencerm@gmail.com>
2018-03-31 20:35:49 -0700

    works on desktop too

--- ocr.py
+++ ocr.py
@@ -5,7 +5,6 @@ import cv2
 import os
 import time
 import urllib.request
-import nltk
 
 M1W = 1
 M2W = 1
@@ -22,19 +21,20 @@ def main(preprocess):
     #args = vars(ap.parse_args())
 
     # load the example image and convert it to grayscale
-    image = cv2.imread("image.jpg")
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
- 
+    # Had issues with opencv needing full image path, hence this discusting code
+    #   finds this file's path and uses it to find image
+    gray = cv2.imread(os.path.realpath(__file__).replace('\\', "/").replace("ocr.py", "") + 'image.jpg', 0)
+
     # check to see if we should apply thresholding to preprocess the
     # image
     if preprocess == "thresh":
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
- 
+
     # make a check to see if median blurring should be done to remove
     # noise
     elif preprocess == "blur":
         gray = cv2.medianBlur(gray, 3)
-     
+
     # write the grayscale image to disk as a temporary file so we can
     # apply OCR to it
     coords = [[288, 804], [864, 988], [1048, 1173], [1223, 1363]]
@@ -74,11 +74,10 @@ def main(preprocess):
         weighted[i] = M3W * m3unweighted[i]/largenum
     print("Method 3: " + str(largest))
     print(weighted)
-        
+
 
     print("Took " + str(time.time() - starttime) + " seconds to  run.");
     # show the output images
-    cv2.imshow("Image", image)
     cv2.imshow("Output", cv2.resize(gray, (200, 350), interpolation = cv2.INTER_CUBIC))
     cv2.waitKey(0)
 
@@ -89,4 +88,3 @@ main("thresh")
 #864-988
 #1048-1173
 #1223-1363
-
